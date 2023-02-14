c738f654dc5bd621e17a8a651493057d1fcfce7d
Grovre <50428844+Grovre@users.noreply.github.com>
2023-02-13 20:11:34 -0600

    Unit testing

---
+++ Testing/Generator.cs
@@ -0,0 +1,24 @@
+﻿namespace Testing;
+
+public static class Generator
+{
+    public delegate T IntFunction<T>(int n);
+    
+    public static IEnumerable<T> GenerateEnumerable<T>(int length, IntFunction<T> indexKeyGenerator)
+    {
+        var array = new T[length];
+        var i = 0;
+        foreach (ref var el in array.AsSpan())
+        {
+            el = indexKeyGenerator(i++);
+        }
+
+        return array;
+    }
+
+    public static IEnumerable<T> GenerateEnumerable<T>(int length, Func<T> keyGenerator)
+        => GenerateEnumerable(length, _ => keyGenerator());
+
+    public static T[] GenerateArray<T>(int length, IntFunction<T> indexKeyGenerator)
+        => GenerateEnumerable(length, indexKeyGenerator).ToArray();
+}
---
+++ Testing/RandomHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Testing;
+
+public static class RandomHelper
+{
+    public static T NextStruct<T>(this Random random) where T : struct
+    {
+        if (RuntimeHelpers.IsReferenceOrContainsReferences<T>())
+            throw new Exception("Cannot generate a struct containing a ref");
+        
+        var o = default(T);
+        random.NextBytes(MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref o, 1)));
+        return o;
+    }
+}
